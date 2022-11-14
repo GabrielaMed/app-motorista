0ffddb20e2292fc28f6ca6fa07b268d268a6bc07
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2022-11-14 16:23:36 -0400

    open photo

--- src/pages/Photo/index.tsx
+++ src/pages/Photo/index.tsx
@@ -89,9 +89,9 @@ export const Photos = () => {
   //   }
   // }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <div
